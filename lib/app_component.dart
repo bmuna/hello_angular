2948ded005b3473167885a7b0218477b8f80e9fe
biruk <brookyonas@gmail.com>
2021-09-02 16:21:20 +0300

    changes

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,35 @@
 import 'package:angular/angular.dart';
+import 'package:angular_app/more_directives/learn_direct_component.dart';
+import 'package:angular_app/smart_component/smart_component.dart';
+import 'package:angular_app/student_firebase_component/student_firebase_component.dart';
+import 'package:angular_app/student_form_component/student_form_component.dart';
+import 'package:angular_app/student_pipe_component/student_pipe_component.dart';
+import 'package:angular_app/student_routing/student_list_component.dart';
+import 'package:angular_app/todo_parse_component/todo_parse_component.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  // template: '<h1>Hellowa {{name}}</h1>',
+  templateUrl: 'app_component.html',
+  directives: [
+    coreDirectives,
+    LearnDirectComponent,
+    SmartComponent,
+    TodoComponent,
+    StudentFirebase,
+    StudentForm,
+    StudentPipe,
+    StudentListComponent
+  ]
 )
 class AppComponent {
-  var name = 'Angular';
+
+  Map<String, dynamic> student = {
+    'name': 'shela',
+    'isSmart': false,
+    'description':'She is very smart',
+  };
+  void onIsSmartChanged(){
+    print('aaaa');
+  }
 }
---
+++ lib/student_routing/student_announcement/student_announcement_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'student-announcement',
+  styleUrls: ['student_announcement_component.css'],
+  template: '<h1> Student Announcement </h1>'
+)
+
+class StudentAnnouncementComponent{
+
+}
